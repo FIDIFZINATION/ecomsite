6c29721be6e42a9b8904cd6d22922b4abf14f29f
fidifzination <fidifzination@gmail.com>
2021-11-08 10:21:08 +0530

    Handled cart and checkout page for logged in users.

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,7 @@
 from typing import DefaultDict
 from django.db import models
 from django.db.models.deletion import CASCADE
+from accounts.models import Account
 from store.models import Product, Variation
 # Create your models here.
 
@@ -12,9 +13,10 @@ class Cart(models.Model):
         return self.cart_id
 
 class CartItem(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=CASCADE)
     variations = models.ManyToManyField(Variation, blank=True)
-    cart = models.ForeignKey(Cart, on_delete=CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
