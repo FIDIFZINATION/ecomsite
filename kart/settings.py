66442b247df556bbf5a7502fc7e6eb903e9c16fd
fidifzi nation <69453680+FIDIFZINATION@users.noreply.github.com>
2023-02-04 15:30:17 +0530

    Update settings.py

--- kart/settings.py
+++ kart/settings.py
@@ -156,6 +156,6 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kaifkazi40@gmail.com'
-EMAIL_HOST_PASSWORD = 'K@ifkazi12'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
